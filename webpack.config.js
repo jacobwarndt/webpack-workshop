af97467c43400dfb2048d23e5c7ad68d94df2175
JacobArndt <jacobwarndt@gmail.com>
2025-06-24 13:42:33 -0400

    chore(webpack): configure webpack with development settings and module rules

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,25 @@
 const path = require('path');
 
-module.exports = {};
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
